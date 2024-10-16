b99fcfe95e96e78dcd1b728eca83625da2e5d2cd
Денис Касперович <kasperovich.deonis@gmail.com>
2024-10-16 13:32:13 +0300

    added a class entity to create players and google

---
+++ entity/Entity.js
@@ -0,0 +1,20 @@
+export class Entity {
+    #position
+    #points
+    constructor() {
+        this.#position = null
+        this.#points = 0
+    }
+    get position(){
+        return this.#position
+    }
+    set position(newPosition){
+        this.#position = newPosition
+    }
+    get points(){
+        return this.#points
+    }
+    set points(updatedPoints){
+        this.#points = updatedPoints
+    }
+}
