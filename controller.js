5694cd610400b0bd16eefa85c9a6fd8575da62b3
Денис Касперович <kasperovich.deonis@gmail.com>
2024-10-17 21:04:45 +0300

    created controller for management view and model (game)

---
+++ controller.js
@@ -0,0 +1,56 @@
+export class Controller {
+    /**
+     * @type View
+     */
+    #view;
+    /**
+     * @type Game
+     */
+    #model;
+
+    // Dependency Injections
+    constructor(view, model) {
+        this.#view = view;
+        this.#model = model;
+        this.#view.onStartGame = (settings) => this.startGame(settings)
+    }
+
+    async init() {
+        this.#model.addEventListener(async () => {
+            await this.#refreshUI()
+        })
+        await this.#refreshUI()
+    }
+
+    async #refreshUI() {
+        const viewModelDTO = await this.mapModelToViewModelDTO()
+        this.#view.render(viewModelDTO);
+    }
+    async mapModelToViewModelDTO() {
+        return {
+            status: await this.#model.getStatus(),
+            gridSize: await this.#model.getSetting('gridSize'),
+            gameEntities: {
+                google: {
+                    position: await this.#model.getPosition('google'),
+                    points: await this.#model.getPoints('google')
+                },
+                player1: {
+                    position: await this.#model.getPosition('player1'),
+                    points: await this.#model.getPoints('player1')
+                },
+                player2:  {
+                    position: await this.#model.getPosition('player2'),
+                    points: await this.#model.getPoints('player2')
+                }
+            }
+        };
+    }
+
+    async startGame(settings){
+        await this.#model.start(settings)
+    }
+
+    //функция хотьбы ('up', 'player1')
+
+}
