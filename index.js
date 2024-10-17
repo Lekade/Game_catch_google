6711087b8de61cb6d7b8aa994ab1d2e2459f6d3a
Денис Касперович <kasperovich.deonis@gmail.com>
2024-10-17 21:02:14 +0300

    added html and js indexes, which are the starting elements when launching the application

---
+++ index.js
@@ -0,0 +1,14 @@
+// entry point
+import {View} from "./view/view.js";
+import {Controller} from "./controller.js";
+import {Game} from "./model/game.js";
+import {NumberUtility} from "./model/number-utility.js";
+
+const numberUtility = new NumberUtility()
+const model = new Game(numberUtility)
+
+const view = new View('app')
+
+const controller = new Controller(view, model)
+
+controller.init();
