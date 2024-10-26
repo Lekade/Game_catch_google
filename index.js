a3aa0e3794e6762187f10821602455bf0c1b2ee3
Денис Касперович <kasperovich.deonis@gmail.com>
2024-10-26 22:29:02 +0300

    using networkController and api-proxy

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 // entry point
-import {View} from "./view/view.js";
-import {Controller} from "./controller.js";
-import {Game} from "./model/game.js";
-import {NumberUtility} from "./model/positionManager/number-utility.js";
+import {View} from "./front/view/view.js";
+import {NetworkController} from "./front/network-controller.js";
+import {APIProxy} from "./front/api-proxy.js";
 
-const numberUtility = new NumberUtility()
-const model = new Game(numberUtility)
 
+const apiProxy = new APIProxy()
 const view = new View('app')
 
-const controller = new Controller(view, model)
+const controller = new NetworkController(view, apiProxy)
 
 controller.init();
